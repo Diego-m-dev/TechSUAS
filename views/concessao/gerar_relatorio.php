0e699de38a5a50b2cd76c2253bba35d86c328826
emmanuelcadete <engmecanicocadete@gmail.com>
2024-03-26 16:51:48 -0300

    1

---
+++ views/concessao/gerar_relatorio.php
@@ -0,0 +1,80 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/dados_operador.php';
+
+$data1 = date('Y');
+$data2 = $data1 - 8;
+$data3 = $data1 - 7;
+$data4 = $data1 - 6;
+$data5 = $data1 - 5;
+$data6 = $data1 - 4;
+$data7 = $data1 - 3;
+$data8 = $data1 - 2;
+$data9 = $data1 - 1;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerar Relatório Concessão</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="/TechSUAS/css/concessao/style_gerar_form.css">
+    <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="/TechSUAS/js/concessao.js"></script>
+</head>
+
+<body>
+
+<form action="" method="POST">
+    <label>Mês de Pagamento</label>
+        <select name="mes_pg" id="mes_pg" required>
+            <option value="" disabled selected hidden>Selecione</option>
+            <option value="Janeiro">Janeiro</option>
+            <option value="Fevereiro">Fevereiro</option>
+            <option value="Março">Março</option>
+            <option value="Abril">Abril</option>
+            <option value="Maio">Maio</option>
+            <option value="Junho">Junho</option>
+            <option value="Julho">Julho</option>
+            <option value="Agosto">Agosto</option>
+            <option value="Setembro">Setembro</option>
+            <option value="Outubro">Outubro</option>
+            <option value="Novembro">Novembro</option>
+            <option value="Dezembro">Dezembro</option>
+        </select>
+
+        <label>ANO</label>
+        <select id="ano_select" name="ano_select" required>
+            <option value="" disabled selected hidden>Selecione</option>
+            <option value="<?php echo $data9; ?>"><?php echo $data9; ?></option>
+            <option value="<?php echo $data1; ?>"><?php echo $data1; ?></option>
+        </select>
+        <button type="submit">GERAR</button>
+</form>
+<?php
+if (!isset($_POST['mes_pg'])){
+} else {
+    $mes_pago = $_POST['mes_pg'];
+    $ano_select = $_POST['ano_select'];
+
+    $stmt_gera_relatorio = $pdo->prepare("SELECT * FROM concessao_historico WHERE ano_form = :ano_form AND mes_pag LIKE :mes_pag");
+    $stmt_gera_relatorio->bindParam(':ano_form', $ano_select, ':mes_pag', $mes_pago, PDO::PARAM_STR);
+    $stmt_gera_relatorio->execute();
+
+    if ($stmt_gera_relatorio->rowCount() > 0) {
+        $dados_historico = $stmt_gera_relatorio->fetch(PDO::FETCH_ASSOC);
+
+        
+    
+    }
+}
+?>
+</body>
+</html>
--- views/concessao/index.php
+++ views/concessao/index.php
@@ -9,10 +9,10 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/dados_operador.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TechSUAS - Concessão</title>
-    <link rel="stylesheet" href="css/style_conc.css">
+    <title>Concessão - TechSUAS</title>
+    <link rel="stylesheet" href="/TechSUAS/css/concessao/style_conc.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="website icon" type="png" href="/Suas-Tech/cadunico/img/logo.png">
+    <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -22,7 +22,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/dados_operador.php';
 
     <div class="img">
         <h1 class="titulo-com-imagem">
-            <img src="img/h1-concessao.svg" alt="Titulocomimagem">
+            <img src="/TechSUAS/img/concessao/h1-concessao.svg" alt="Titulocomimagem">
         </h1>
     </div>
         <div class="apelido">
@@ -34,7 +34,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/dados_operador.php';
             <div class="menu">
                 <nav>
                     <div class="btn">
-                        <a class="menu-button" id="cadastrar_contrato" onclick="location.href='views/cadastro_pessoa';">
+                        <a class="menu-button" id="cadastrar_contrato" onclick="location.href='cadastro_pessoa';">
                             <span class="material-symbols-outlined">
                             person_add
                             </span>
@@ -42,7 +42,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/dados_operador.php';
                         </a>
                     </div>
                     <div class="btn">
-                        <a class="menu-button" id="cadastrar_contrato" onclick="location.href='/Suas-Tech/concessao/views/cadastro_item';">
+                        <a class="menu-button" id="cadastrar_contrato" onclick="location.href='cadastro_item';">
                             <span class="material-symbols-outlined">
                             add_box
                             </span>
@@ -50,7 +50,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/dados_operador.php';
                         </a>
                     </div>
                     <div class="btn">
-                        <a class="menu-button" id="cadastrar_contrato" onclick="window.location.href='views/editar'">
+                        <a class="menu-button" id="cadastrar_contrato" onclick="window.location.href='consultar'">
                             <span class="material-symbols-outlined">
                             content_paste_search
                             </span>
@@ -58,11 +58,19 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/dados_operador.php';
                         </a>
                     </div>
                     <div class="btn">
-                        <a class="menu-button" id="cadastrar_contrato" onclick="location.href='views/gerar_form';">
+                        <a class="menu-button" id="cadastrar_contrato" onclick="location.href='gerar_form';">
                             <span class="material-symbols-outlined">
                             inventory
                             </span>
                             Gerar Formulário 
+                        </a>
+                    </div>
+                    <div class="btn">
+                        <a class="menu-button" id="cadastrar_contrato" onclick="location.href='gerar_relatorio';">
+                            <span class="material-symbols-outlined">
+                            inventory
+                            </span>
+                            Gerar Relatório
                         </a>
                     </div>
                 </nav>
@@ -75,7 +83,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/dados_operador.php';
         </div>
 
         </div>
-        <footer><img src="../suas/views/adm/img/footer-adm.svg" alt=""></footer>
+        <footer><img src="/TechSUAS/img/geral/footer-adm.svg" alt=""></footer>
         <div class="drop-all">
             <div class="menu-drop">
                 <button class="logout" type="button" name="drop">
@@ -83,15 +91,15 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/dados_operador.php';
                         Settings
                     </span>
                     <div class="drop-content">
-                        <a title="Sair" href='/Suas-Tech/config/logout' ;>
+                        <a title="Sair" href='/TechSUAS/config/logout' ;>
                             <span title="Sair" class="material-symbols-outlined">logout</span>
                         </a>
-                        <a title="Alterar Usuário" href='/Suas-Tech/cras/views/conta' ;>
+                        <a title="Alterar Usuário" href='/TechSUAS/views/geral/conta' ;>
                             <span class="material-symbols-outlined">manage_accounts</span>
                         </a>
                         <?php
 if ($nivel == 'suport') {
-    ?> <a title="Suporte" href='/Suas-Tech/acesso_suporte/index' ;>
+    ?> <a title="Suporte" href='/TechSUAS/suporte/index' ;>
                                 <span class="material-symbols-outlined">rule_settings</span>
                             </a> <?php
 exit();
