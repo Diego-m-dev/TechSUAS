00c82b7f58893d63c3e2c7a8c00425140fc19e90
Diego-m-dev <diegomoreira.pab@gmail.com>
2024-03-28 16:15:27 -0300

    1

--- views/cadunico/forms/termo_responsabilidade.php
+++ views/cadunico/forms/termo_responsabilidade.php
@@ -10,7 +10,6 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/TechSUAS/css/cadunico/forms/tr.css">
-    <script src="../../js/bd_tr.js"></script>
     <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
 </head>
 <body>
