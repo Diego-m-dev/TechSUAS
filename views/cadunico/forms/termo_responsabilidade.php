22635ba43627b39a7a9987dac482b1e6f695a402
Diego-m-dev <diegomoreira.pab@gmail.com>
2024-03-28 15:28:43 -0300

    1

--- views/cadunico/forms/termo_responsabilidade.php
+++ views/cadunico/forms/termo_responsabilidade.php
@@ -1,5 +1,5 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/sessao.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
 ?>
 
 
@@ -9,10 +9,9 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/sessao.php';
     <title>Termo de Responsabilidade</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SYCAD</title>
-    <link rel="stylesheet" href="../../css/formularios/tr.css">
+    <link rel="stylesheet" href="/TechSUAS/css/cadunico/forms/tr.css">
     <script src="../../js/bd_tr.js"></script>
-    <link rel="website icon" type="png" href="../../img/logo.png">
+    <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
 </head>
 <body>
     <br>
