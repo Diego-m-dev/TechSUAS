bf3aa9682643f31bf4c6f7d9c22041c8e37ce281
emmanuelcadete <engmecanicocadete@gmail.com>
2024-04-17 11:04:38 -0300

    parceria

--- config/autenticar.php
+++ config/autenticar.php
@@ -1,8 +1,17 @@
+<?php
+require "../controller/geral/secret.php";
+
+session_set_cookie_params(['httponly' => true]);
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/TechSUAS/css/cadunico/style_index_cad">
+    <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
@@ -12,78 +21,93 @@
 require_once "conexao.php";
 session_start();
 
+session_regenerate_id(true);
+
 $usuario = $_POST['usuario'];
 $senha_login = $_POST['senha'];
 
-$stmt = $pdo->prepare("SELECT * FROM usuarios WHERE usuario = :usuario");
-$stmt->bindValue(":usuario", $usuario);
-$stmt->execute();
+$stmt_login = $pdo->prepare("SELECT * FROM usuarios WHERE usuario = :usuario");
+$stmt_login->bindValue(":usuario", $usuario);
+$stmt_login->execute();
 
-$dados = $stmt->fetch(PDO::FETCH_ASSOC);
+$dados = $stmt_login->fetch(PDO::FETCH_ASSOC);
 if ($dados && is_array($dados) && array_key_exists('setor', $dados)) {
-$setor_ = $dados['setor'];
-$senhalogin = $dados['acesso'];
+    $setor_ = $dados['setor'];
+    $senhalogin = $dados['senha'];
 
-if ($dados && password_verify($senha_login, $dados['senha'])) {
-    $_SESSION['nome_usuario'] = $dados['nome'];
-    $_SESSION['user_usuario'] = $dados['usuario'];
-    $_SESSION['nivel_usuario'] = $dados['nivel'];
+    if ($usuario == $dados['usuario'] && password_verify($senha_login, $dados['senha'])) {
+        $_SESSION['nome_usuario'] = $dados['nome'];
+        $_SESSION['user_usuario'] = $dados['usuario'];
+        $_SESSION['nivel_usuario'] = $dados['nivel'];
+        $_SESSION['apelido'] = $dados['apelido'];
+        $_SESSION['setor'] = $dados['setor'];
 
-        // Verifique se é o primeiro acesso pela senha
-        if ($senhalogin == "1") {
-            // Passa o nome do usuário para a página de primeiro acesso
-            $_SESSION['nome_user_1_acesso'] = $dados['usuario'];
-    
-            // Redirecione para a página de conclusão do cadastro
-            header("Location: ../cadunico/views/acessos/primeiro_acesso");
+        if ($dados['acesso'] == 1) {
+            header("location:/TechSUAS/views/geral/primeiro_acesso");
             exit();
         }
 
-    if ($setor_ == "CRIANÇA FELIZ"){
-        //header("location:../suas/peixe/logado/form");
-        exit();
-    }
-    // Redirecione com base no nível de acesso
-    if ($_SESSION['nivel_usuario'] == 'suport') {
-        header("location:/TechSUAS/suporte/index");
-        exit();
-    } elseif ($setor_ == "CRAS - ANTONIO MATIAS") {
-        header("Location: ../cras/views/menu-cras-am");
-        exit();
-    } elseif ($setor_ == "CADASTRO UNICO - SECRETARIA DE ASSISTENCIA SOCIAL") {
-        if ($_SESSION['nivel_usuario'] == 'admin') {
-            header("location:../cadunico/painel-adm/adm-view");
+        // Redirecione com base no nível de acesso
+        if ($_SESSION['nivel_usuario'] == 'suport') {
+            header("location:/TechSUAS/suporte/");
+            exit();
+        } elseif ($_SESSION['nivel_usuario'] == 'admin') {
+            if ($_SESSION['setor'] == "CADASTRO ÚNICO - SECRETARIS DE ASSISTÊNCIA SOCIAL") {
+                header("location:/TechSUAS/views/cadunico/");
+            } elseif ($_SESSION['setor'] == "SUPORTE") {
+                header("location:/TechSUAS/suporte/");
+            }
             exit();
         } elseif ($_SESSION['nivel_usuario'] == 'usuario') {
-            header("location:../cadunico/painel-usuario/user-painel");
+            if ($_SESSION['setor'] == "CONCESSÃO") {
+                header("location:/TechSUAS/views/concessao/");
+            } elseif ($_SESSION['setor'] == 'ADMINISTRATIVO E CONCESSÃO') {
+                ?>
+                    <script>
+                        Swal.fire({
+                            html:`
+                            <h2>PERFIL PERMISSIONADO PARA DOIS SETORES</h2>
+            <div class="visitas">
+                <a class="menu-button" onclick="location.href='/TechSUAS/views/concessao/';">
+                    CONCESSÃO
+                </a>
+            </div>
+
+            <div class="folha">
+                <a class="menu-button" onclick="location.href='/TechSUAS/views/administrativo/';">
+                    ADMINISTRATIVO
+                </a>
+            </div>
+        `,
+            }).then((result) => {
+                if(result.isConfirmed) {
+                    window.location.href = "/TechSUAS/config/logout.php";
+                }
+            })
+        </script>
+                <?php
+            }
             exit();
-        }
-    } elseif ($setor_ == "CRAS - SANTO AFONSO") {
-        header("Location: ../cras/views/menu-cras-st");
-    } elseif ($setor_ == "CREAS - GILDO SOARES") {
-        header("Location: ../creas/views/menu-creas");
-    } elseif ($setor_ == "COZINHA COMUNITARIA - MARIA NEUMA DA SILVA") {
-        header("Location: ../cozinha_comunitaria/menu");
-    }else{
-        ?>
+        } else {
+            ?>
         <script>
             Swal.fire({
-            icon: "error",
-            title: "SENHA INCORRETA",
-            text: "Usuário ou senha não condiz com a base de dados.",
+            icon: "info",
+            title: "SEM PERMISSÃO",
+            text: "Você ainda não foi permissionado.",
             confirmButtonText: 'OK',
             }).then((result) => {
                 if (result.isConfirmed) {
-                    window.location.href = "/TechSUAS/index";
+                    window.location.href = "/TechSUAS/";
                 }
             });
         </script>
-            <?php
-    }
+<?php
+}
 
     } else {
 
-    ?>
+        ?>
 <script>
     Swal.fire({
     icon: "error",
@@ -97,18 +121,18 @@ if ($dados && password_verify($senha_login, $dados['senha'])) {
     });
 </script>
     <?php
-    }
+}
 } else {
     ?>
 <script>
     Swal.fire({
     icon: "error",
-    title: "SENHA INCORRETA",
-    text: "Usuário ou senha não condiz com a base de dados.",
+    title: "DADOS VAZIO",
+    text: "Não localizado.",
     confirmButtonText: 'OK',
     }).then((result) => {
         if (result.isConfirmed) {
-            window.location.href = "/TechSUAS/index.php";
+            window.location.href = "/TechSUAS/index";
         }
     });
 </script>
