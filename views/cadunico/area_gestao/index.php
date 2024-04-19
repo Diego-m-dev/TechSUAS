0bc134b8a80a22a39415a20fef4675acb803b194
emmanuelcadete <engmecanicocadete@gmail.com>
2024-04-19 13:51:28 -0300

    dew

--- views/cadunico/area_gestao/index.php
+++ views/cadunico/area_gestao/index.php
@@ -1,59 +1,55 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/permissao_cadunico.php';
+if ($_SESSION['funcao'] != '1') {
+    echo '<script>window.history.back()</script>';
+    exit();
+}
+ini_set('memory_limit', '8192M');
+ini_set('max_execution_time', 300);
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/TechSUAS/css/cadunico/style_ag_cad.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="/TechSUAS/js/cadastro_unico.js"></script>
     <title>Área Gestor</title>
 </head>
 <body>
-    <canvas id="myChart"></canvas>
+    <h1>AREA DO GESTOR</h1>
+        <form method="post">
+            <label for="">NOME:</label>
+            <input type="text" name="nome_pessoa"/>
+            <label for="">Código Familiar</label>
+            <input type="text" name="codigo_familia">
+            <button type="submit" name="btn_filtro">Buscar</button>
+        </form>
+    <?php
 
-    <script>
-        // Recuperar os dados do PHP
-        var data = <?php echo json_encode($data); ?>;
+if (!isset($_POST['btn_filtro'])) {
 
-        // Processar os dados para o gráfico
-        var labels = [];
-        var dataValues = [];
+} else {
+    if ($_POST['nome_pessoa']){
+        $filtro = $conn->real_escape_string($_POST['nome_pessoa']);
+    } elseif ($_POST['codigo_familia']) {
+        $filtro = $conn->real_escape_string($_POST['codigo_familia']);
+    }
+    $smtp_filtros = $conn->prepare("SELECT * FROM tbl_tudo WHERE cod_familiar_fam LIKE '%$filtro' OR nom_pessoa LIKE '%$filtro%'");
+    $smtp_filtros->execute();
 
-        data.forEach(function(item) {
-            if (labels.includes(item.IN_PROCESSO)) {
-                var index = labels.indexOf(item.IN_PROCESSO);
-                dataValues[index]++;
-            } else {
-                labels.push(item.IN_PROCESSO);
-                dataValues.push(1);
-            }
-        });
+    $result = $smtp_filtros->get_result(); // Obter o resultado da consulta
+    $dados_filtro = $result->fetch_all(MYSQLI_ASSOC); // Obter todas as linhas como uma matriz associativa
 
-        // Criar o gráfico
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Quantidade de Cadastros',
-                    data: dataValues,
-                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                    borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    </script>
+    foreach ($dados_filtro as $dados) {
+        echo $dados['cod_parentesco_rf_pessoa']. ' - ' . $dados['cod_familiar_fam'] . " " . $dados['nom_pessoa'] . " - " . $dados['num_nis_pessoa_atual'] ."<br>";
+    }
+}
+?>
 </body>
 </html>
