f600f64334bee89133519d273c296394ed36fa7b
Diego-m-dev <diegomoreira.pab@gmail.com>
2024-04-22 10:21:51 -0300

    1

---
+++ views/cadunico/links.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Links Úteis</title>
+    <link rel="stylesheet" href="/TechSUAS/css/cadunico/style_link">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+</head>
+
+<body>
+    <div class="img">
+        <h1 class="titulo-com-imagem">
+            <img class="titulo-com-imagem" src="/TechSUAS/img/cadunico/h1-links.svg" alt="Titulocomimagem">
+        </h1>
+    </div>
+
+    <nav>
+        <a href="https://www.beneficiossociais.caixa.gov.br" target="_blank">SIBEC</a></div>
+        <a href="http://cadastrounico.caixa.gov.br" target="_blank" class="cadunico">CADASTRO ÚNICO</a>
+        <a href="https://aplicacoes.cidadania.gov.br/sagi/autenticacao/login.php" target="_blank" class="cecad">CECAD</a>
+        <a href="https://www.mds.gov.br/mds-sigpbf-web/carregarTelaLogin.jsf;jsessionid=fdL4CJM3HzqgPmqgvb3i9aKrL02hrOuEBCb9dQo9.susigpbfpd01" target="_blank" class="sigpbf">SIGPBF</a>
+        <a href="https://cadunico.dataprev.gov.br/portal/" target="_blank" class="pcadunico">PORTAL CADASTRO ÚNICO</a>
+        <a href="https://falemds.centralit.com.br/formulario/" target="_blank" class="pcadunico">FORMULÁRIO ELETRÔNICO DO MDS</a>
+        <a href="/TechSUAS/config/back.php">
+            <i class="fas fa-arrow-left"></i> Voltar ao menu</a>
+    </nav>
+
+    <div class="num">
+        <span>
+            <h2> <span class="material-symbols-outlined">
+                    call_quality
+                </span>Canais de atendimento</h2>
+            <p>Caixa Economica Federal<br>Dúvidas Operacionais
+                4004 0104 (capitais e regiões metropolitanas)
+                <br>0800 104 0104 (demais cidades)<br>Atendimento 24h
+            </p>
+            <p>Ouvidoria Portal Cadastro Único <br>
+                0800 725 7474 <br>
+                Atendimento em dias úteis - das 09h às 18h</p>
+            <p>MDS<br>121 (Horário de atendimento:
+                segunda a sexta-feira, das 7h às 19h)</p>
+        </span>
+</body>
+
+</html>
