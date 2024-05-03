f53c098cbfa00749065f395f54df8cafef74924e
emmanuelcadete <engmecanicocadete@gmail.com>
2024-05-03 15:06:51 -0300

    MARIA

--- config/autenticar.php
+++ config/autenticar.php
@@ -8,7 +8,7 @@ session_set_cookie_params(['httponly' => true]);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/TechSUAS/css/cadunico/style_index_cad">
+    <link rel="stylesheet" href="/TechSUAS/css/cadunico/style_index_cad.css">
     <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -68,7 +68,7 @@ if ($dados && is_array($dados) && array_key_exists('setor', $dados)) {
                     <script>
                         Swal.fire({
                             html:`
-                            <h2>PERFIL PERMISSIONADO PARA DOIS SETORES</h2>
+            <h2>OPERADOR PERMISSIONADO PARA DOIS SETORES</h2>
             <div class="visitas">
                 <a class="menu-button" onclick="location.href='/TechSUAS/views/concessao/';">
                     CONCESSÃO
@@ -81,7 +81,9 @@ if ($dados && is_array($dados) && array_key_exists('setor', $dados)) {
                 </a>
             </div>
         `,
-            }).then((result) => {
+        showCancelButton: false,
+        showConfirmButton: false,
+                }).then((result) => {
                 if(result.isConfirmed) {
                     window.location.href = "/TechSUAS/config/logout";
                 }
