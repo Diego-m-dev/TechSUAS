6f4b5a2442d5ab990feaafc88a6dc93938933f84
emmanuelcadete <engmecanicocadete@gmail.com>
2024-05-03 15:06:43 -0300

    RF

--- index.php
+++ index.php
@@ -52,30 +52,30 @@
     </div>
 
     <footer class="rodape">
-        Sistema desenvolvido por DDV &trade; &nbsp;&nbsp;&nbsp;&nbsp; Versão 2.1.1
+        Sistema desenvolvido por DDV &trade; &nbsp;&nbsp;&nbsp;&nbsp; Versão 5.2.35
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            const mostrarSenha = document.getElementById('mostrar-senha');
-            const campoSenha = document.getElementById('senha');
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const mostrarSenha = document.getElementById('mostrar-senha')
+        const campoSenha = document.getElementById('senha')
 
-            mostrarSenha.addEventListener('click', function () {
-                if (campoSenha.getAttribute('type') === 'password') {
-                    campoSenha.setAttribute('type', 'text');
-                    mostrarSenha.classList.remove('fa-eye');
-                    mostrarSenha.classList.add('fa-eye-slash');
-                } else {
-                    campoSenha.setAttribute('type', 'password');
-                    mostrarSenha.classList.remove('fa-eye-slash');
-                    mostrarSenha.classList.add('fa-eye');
-                }
-            });
-        });
-    </script>
+        mostrarSenha.addEventListener('click', function () {
+            if (campoSenha.getAttribute('type') === 'password') {
+                campoSenha.setAttribute('type', 'text')
+                mostrarSenha.classList.remove('fa-eye')
+                mostrarSenha.classList.add('fa-eye-slash')
+            } else {
+                campoSenha.setAttribute('type', 'password')
+                mostrarSenha.classList.remove('fa-eye-slash')
+                mostrarSenha.classList.add('fa-eye')
+            }
+        })
+    })
+</script>
 </body>
 </html>
