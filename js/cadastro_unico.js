0b82524adb3d7a19fc21198be7659d8a45b3f0aa
emmanuelcadete <engmecanicocadete@gmail.com>
2024-05-03 22:19:44 -0300

    mior

--- js/cadastro_unico.js
+++ js/cadastro_unico.js
@@ -1,4 +1,4 @@
-$(document).ready( function () {
+$(document).ready(function () {
     $('#simples').hide()
     $('#beneficio').hide()
     $('#btn_familia').click(function () {
@@ -10,4 +10,61 @@ $(document).ready( function () {
         $('#simples').hide()
     })
 
+
 })
+
+$(document).ready(function () {
+    $('#btn_residencia').click(function () {
+        Swal.fire({
+            title: "TERMO DE RESPONSABILIDADE",
+            html: `
+            <h4>INFORME O CPF</h4>
+            <form method="POST" action="termo_responsabilidade" id="form_residencia">
+                <label> CPF:
+                    <input type="text" name="cpf_residencia"/>
+                </label>
+            </form>
+            `,
+            showCancelButton: true,
+            confirmButtonText: 'Enviar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const form = document.getElementById("form_residencia")
+                form.submit()
+            }
+        })
+    })
+
+    $('#btn_dec_renda').click(function () {
+        Swal.fire({
+            title: "TERMO DE DECLARAÇÃO",
+            html: `
+            <h4>INFORME O CPF</h4>
+            <form method="POST" action="Termo_declaracao" id="form_dec_renda">
+                <label> CPF:
+                    <input type="text" name="cpf_declar"/>
+                </label>
+            </form>
+            `,
+            showCancelButton: true,
+            confirmButtonText: 'Enviar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const form = document.getElementById("form_dec_renda")
+                form.submit()
+            }
+        })
+    })
+
+})
+
+
+function voltarAoMenu() {
+    window.location.href = '/TechSUAS/config/back'
+}
+
+function imprimirPagina() {
+    window.print()
+}
