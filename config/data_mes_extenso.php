2f80cca5acbac0e11a3b3edbac0309168b1ec736
emmanuelcadete <engmecanicocadete@gmail.com>
2024-05-03 22:19:34 -0300

    mior

---
+++ config/data_mes_extenso.php
@@ -0,0 +1,13 @@
+<?php
+//formatando data para um formato 'd de mes de ano'
+$data_atual = date('d/m/Y');
+$date_formatando = DateTime::createFromFormat('d/m/Y', $data_atual);
+$formatter = new IntlDateFormatter(
+    'pt_BR', // Localidade
+    IntlDateFormatter::FULL, // Estilo da data
+    IntlDateFormatter::NONE, // Estilo do tempo
+    'America/Sao_Paulo', // Fuso horário
+    IntlDateFormatter::GREGORIAN // Calendário
+);
+$formatter->setPattern('d \'de\' MMMM \'de\' y');
+$data_formatada = $formatter->format($date_formatando);
