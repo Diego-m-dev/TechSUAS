934534fcad8cc1791bb24e8984dd8bdaf43ddd3c
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-02 02:22:33 -0300

    conexao não sessão

--- controller/concessao/print_conc.php
+++ controller/concessao/print_conc.php
@@ -1,5 +1,5 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/conexao.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
 
 header('Content-Type: application/json'); // Define o tipo de conteúdo como JSON
 
