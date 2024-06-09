57b4f72fb2e2ec9d5356c00ad157a8141d1d1563
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-09 20:13:00 -0300

    agora encerrei

--- controller/cadunico/parecer/reprint_visita.php
+++ controller/cadunico/parecer/reprint_visita.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
-    <link rel="stylesheet" href="/TechSUAS/css/cadunico/visitas/style_reprint.css">
+    <link rel="stylesheet" href="/TechSUAS/css/cadunico/visitas/style_print_visita.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -162,7 +162,9 @@
 
             <h4>Resumo da visita</h4>
 <?php
-                    echo '<span id="resumo_visita">' . $dadosv['resumo_visita'] . '</span>';
+        $texto_paragrafo = str_replace("<br />", '</p><p id="resumo_visita">', $dadosv['resumo_visita']);
+        echo '<p id="resumo_visita">' . $texto_paragrafo . '</p>';
+        //echo '<span id="resumo_visita">' . $dadosv[''] . '</span>';
     }
 }
 ?>
