77da470be579db6e61728ea890a9a1d5dc754c78
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-18 01:32:48 -0300

    já avisei que vai dar merda

--- config/conexao.php
+++ config/conexao.php
@@ -1,5 +1,5 @@
 <?php
-$host = getenv('DB_HOST') ?:'localhost';
+$host = getenv('DB_HOST') ?: 'localhost';
 $usuario = getenv('DB_USER') ?: 'root';
 $senha = getenv('DB_PASS') ?: '';
 $banco = getenv('DB_NAME') ?: 'test';
@@ -10,20 +10,19 @@ $raiz_dom = "/TechSUAS/";
 date_default_timezone_set('America/Sao_Paulo');
 
 try {
-    $pdo = new PDO("mysql:dbname=$banco;host=$host;port=$port", $usuario, $senha, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ]);
-    // conexão mysql para o backup
-    $conn = new mysqli($host, $usuario, $senha, $banco, $port);
+  $pdo = new PDO("mysql:dbname=$banco;host=$host;port=$port", $usuario, $senha, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+  ]);
+  // conexão mysql para o backup
+  $conn = new mysqli($host, $usuario, $senha, $banco, $port);
 
-    // Verifique a conexão.
-    if ($conn->connect_error) {
-        throw new Exception("Falha na conexão com o banco de dados: " . $conn->connect_error);
-    }
+  // Verifique a conexão.
+  if ($conn->connect_error) {
+    throw new Exception("Falha na conexão com o banco de dados: " . $conn->connect_error);
+  }
 } catch (Exception $e) {
-    error_log($e->getMessage(), 3, '/path/to/your/logs/error.log');
-    die("Erro ao conectar com o banco de dados! Por favor, tente novamente mais tarde.");
+  error_log($e->getMessage(), 3, '/path/to/your/logs/error.log');
+  die("Erro ao conectar com o banco de dados! Por favor, tente novamente mais tarde.");
 }
-?>
