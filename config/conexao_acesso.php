43f5fb451c2e3a2a119470688baac064d411a45c
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-21 08:52:21 -0300

    fds

--- config/conexao_acesso.php
+++ config/conexao_acesso.php
@@ -1,26 +1,28 @@
 <?php
 // Conecte-se ao seu banco de dados MySQL usando as credenciais adequadas.
-$host = 'localhost';
-$usuario = 'root';
-$senha = '';
-$banco = 'ddv_suporte';
+$host = '89.117.7.52';
+$usuario = 'u198416735_suporte_ddv';
+$senha = '#Tech2024';
+$banco = 'u198416735_ddv_cont';
 $port = 3306;
 
 $raiz_dom= "/TechSUAS/";
 
 date_default_timezone_set('America/Sao_Paulo');
 try {
-$pdo = new PDO("mysql:dbname=$banco;host=$host", "$usuario", "$senha");
-//conexao mysql para o backyp
-$conn = mysqli_connect($host, $usuario, $senha, $banco);
-} catch (Exception $e) {
-echo "Erro ao conectar com o banco de dados! ".$e;
-}
-
-// Verifique a conexão.
-if ($conn->connect_error) {
-    die("Falha na conexão com o banco de dados: " . $mysqli->connect_error);
-}else{
+    // Adicione a porta à string DSN
+    $pdo = new PDO("mysql:dbname=$banco;host=$host;port=$port", "$usuario", "$senha", array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ));
+    // Conexão MySQLi para o backup
+    $conn = mysqli_connect($host, $usuario, $senha, $banco, $port);
     
+    if ($conn->connect_error) {
+        die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+    } else {
+    }
+} catch (Exception $e) {
+    echo "Erro ao conectar com o banco de dados! " . $e->getMessage();
 }
 ?>
