317fc545337decf140fe2123af38f2347b9bcbb0
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-22 13:49:39 -0300

    poli

--- suporte/index.php
+++ suporte/index.php
@@ -1,6 +1,6 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
-if ($_SESSION['setor'] != "SUPORTE") {
+if ($_SESSION['funcao'] != "0") {
     echo "VOCÊ NÃO TEM PERMISSÃO PARA ACESSAR AQUI!";
     exit();
 }
