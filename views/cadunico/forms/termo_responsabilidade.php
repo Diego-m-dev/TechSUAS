0d913e8898cc590c8ed256cee0c63cf8bde90f38
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-22 13:43:12 -0300

    terminho

--- views/cadunico/forms/termo_responsabilidade.php
+++ views/cadunico/forms/termo_responsabilidade.php
@@ -1,5 +1,5 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
+
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/permissao_cadunico.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/data_mes_extenso.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/basedata.php';
