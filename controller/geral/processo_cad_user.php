a94f9bd203936231922d94c510493cfa76415ca1
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-23 14:10:31 -0300

    diz ae

--- controller/geral/processo_cad_user.php
+++ controller/geral/processo_cad_user.php
@@ -68,32 +68,9 @@ exit();
             die("Sistema não encontrado.");
         }
     } else {
-        $stmt_munic = $pdo_1->prepare("SELECT id FROM sistemas WHERE cpf = :cpf_coord");
-        $stmt_munic->bindValue(":cpf_coord", $_SESSION['cpf']);
-        $stmt_munic->execute();
-
-        $stmt_municipio = $pdo_1->prepare("SELECT id FROM municipios WHERE id = :idm");
-        $stmt_municipio->bindValue(":idm", $id_muncipio);
-        $stmt_municipio->execute();
-
-        if ($stmt_municipio->rowCount() != 0) {
-            $dados_municipio = $stmt_municipio->fetch(PDO::FETCH_ASSOC);
-            $municipio = $dados_municipio['cod_ibge'];
-
-        } else {
-            // Trate o caso em que o município não foi encontrado
-            die("Sistema não encontrado.");
-        }
-
-        if ($stmt_munic->rowCount() != 0) {
-            $dados_munic = $stmt_munic->fetch(PDO::FETCH_ASSOC);
-            $municipio_id = $dados_munic['setores_id'];
-
-        } else {
-            // Trate o caso em que o município não foi encontrado
-            die("Sistema não encontrado.");
-        }
+        $municipio = $cpf_coord;
     }
+    
     // Caso o Nome do Usuário seja unico será adicionado ao SQL
     $smtp = $conn_1->prepare("INSERT INTO operadores (municipio, nome, usuario, senha, setor, funcao, email, acesso, sistema_id) VALUES (?,?,?,?,?,?,?,?,?)");
 
