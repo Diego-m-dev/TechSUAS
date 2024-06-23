f256ec255c4d9019290ffd813638c5ec50ac1d0c
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-23 02:30:19 -0300

    seda

--- config/sessao.php
+++ config/sessao.php
@@ -1,15 +1,15 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 include "conexao_acesso.php";
-// Verifica se o usuário está autenticado como admin ou usuário ou suporte
+
 if (!isset($_SESSION['nome_usuario'])) {
-    $sistemando = "";
     // Configurar o tipo de alerta (success, error, warning, etc.)
     ?>
-
     <!DOCTYPE html>
     <html lang="pt-br">
-
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,12 +19,9 @@ if (!isset($_SESSION['nome_usuario'])) {
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     </head>
-
     <body>
-
     <script>
         document.addEventListener("DOMContentLoaded", function () {
-            // Exemplo de uso do SweetAlert2
             Swal.fire({
                 title: 'Você não está logado. Por favor, faça login.',
                 icon: 'error',
@@ -36,22 +33,27 @@ if (!isset($_SESSION['nome_usuario'])) {
             })
         })
     </script>
-<?php
+    </body>
+    </html>
+    <?php
     exit; // Encerra o script após exibir o alerta
 } else {
-    $stmt_sistma = $pdo_1->prepare("SELECT * FROM sistemas WHERE id = :sis_id");
-    $stmt_sistma->bindValue(":sis_id", $_SESSION['sistema_id'], PDO::PARAM_INT);
-    $stmt_sistma->execute();
+    // Consulta ao banco de dados para buscar informações do sistema e setor
+    $stmt_sistema = $pdo_1->prepare("SELECT * FROM sistemas WHERE id = :sis_id");
+    $stmt_sistema->bindValue(":sis_id", $_SESSION['sistema_id'], PDO::PARAM_INT);
+    $stmt_sistema->execute();
 
-    if ($dado_sys = $stmt_sistma->fetch(PDO::FETCH_ASSOC)) {
-        $sistema = $dado_sys['id'];
+    
+    if ($dado_sys = $stmt_sistema->fetch(PDO::FETCH_ASSOC)) {
+        $sistema = $dado_sys['setores_id'];
 
         $stmt_setor = $pdo_1->prepare("SELECT * FROM setores WHERE id = :sis_id");
         $stmt_setor->bindValue(":sis_id", $sistema, PDO::PARAM_INT);
         $stmt_setor->execute();
 
-        if ($dados_sys = $stmt_setor->fetch(PDO::FETCH_ASSOC)) {
-            $sistemando = $dados_sys['instituicao']. ' - '. $dados_sys['nome_instit'];
+        if ($dados_setor = $stmt_setor->fetch(PDO::FETCH_ASSOC)) {
+            $sistemando = $dados_setor['instituicao'] . ' - ' . $dados_setor['nome_instit'];
+            $id_municipio = $dados_setor['municipio_id'];
         }
     }
 }
