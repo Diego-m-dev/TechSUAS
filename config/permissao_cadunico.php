4c738f1b1879ecede2f96483f4f58c0f05266b2e
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-26 11:50:25 -0300

    wek

--- config/permissao_cadunico.php
+++ config/permissao_cadunico.php
@@ -6,7 +6,7 @@ if ($_SESSION['funcao'] == "0"){
 } else {
     if ($_SESSION['name_sistema'] != "CADUNICO" && $_SESSION['funcao'] != "0") {
     echo "VOCÊ NÃO TEM PERMISSÃO PARA ACESSAR ESSA TELA.";
-
+      echo $_SESSION['name_sistema'];
     exit();
     }
 }
