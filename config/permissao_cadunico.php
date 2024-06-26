6ed6fa45d9308969764a43d8fd914d9a7cd54e48
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-26 07:55:00 -0300

    adcas

--- config/permissao_cadunico.php
+++ config/permissao_cadunico.php
@@ -4,7 +4,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
 if ($_SESSION['funcao'] == "0"){
 
 } else {
-    if ($_SESSION['setor'] != $sistemando && $_SESSION['setor'] != "SUPORTE") {
+    if ($_SESSION['name_sistema'] != "CADUNICO" && $_SESSION['funcao'] != "0") {
     echo "VOCÊ NÃO TEM PERMISSÃO PARA ACESSAR ESSA TELA.";
 
     exit();
