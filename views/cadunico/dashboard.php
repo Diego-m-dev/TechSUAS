bdb017974a983720bc5bafc75c97b719d43b1576
emmanuelcadete <engmecanicocadete@gmail.com>
2024-06-30 17:51:40 -0300

    precioso

--- views/cadunico/dashboard.php
+++ views/cadunico/dashboard.php
@@ -1,5 +1,6 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/conexao.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/permissao_cadunico.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/data_mes_extenso.php';
 ?>
@@ -10,11 +11,12 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/data_mes_extenso.php'
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
-  <link rel="stylesheet" href="/TechSUAS/css/cadunico/style-painel_entrevistador.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+  <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png" />
+  <link rel="stylesheet" href="/TechSUAS/css/cadunico/style-painel_entrevistador.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
@@ -46,7 +48,6 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/data_mes_extenso.php'
           <div id="cont_data">
             <label for="data_entrevista">Data da Entrevista:</label>
             <input type="date" id="data_entrevista" name="data_entrevista">
-            <button type="button" onclick="dataHoje()">Hoje</button>
           </div>
           <div id="data_entre" class="ocult"></div>
         </div>
