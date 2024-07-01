5eac88e5823b69541c7ea2ab336a76a738733aa8
emmanuelcadete <engmecanicocadete@gmail.com>
2024-07-01 17:46:36 -0300

    awerfgedrf

---
+++ config/footer.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/permissao_cadunico.php';
+
+if ($_SESSION['municipio'] == '9876543') {
+  ?>
+  <footer>
+    <div class="municipio"><img src="" alt="noPhoto"></div>
+    <div class="rodape"></div>
+  </footer>
+  <?php
+} elseif ($_SESSION['municipio'] == '2613008') {
+  ?>
+  <footer>
+    <div class="municipio"><img src="/TechSUAS/img/cadunico/logo_municipios/sbu.png" alt=""></div>
+    <div class="rodape"></div>
+  </footer>
+  <?php
+}
