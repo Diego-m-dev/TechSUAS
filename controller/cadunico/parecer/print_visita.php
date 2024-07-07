1a0395cf5743f56012f173c7863ebe3c70f0046f
emmanuelcadete <engmecanicocadete@gmail.com>
2024-07-07 17:57:36 -0300

    boa

--- controller/cadunico/parecer/print_visita.php
+++ controller/cadunico/parecer/print_visita.php
@@ -1,6 +1,7 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/conexao.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/permissao_cadunico.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/data_mes_extenso.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/controller/cadunico/declaracao/create_moth.php';
 ?>
