1948729cb23d3e925ce1f28803ccae29a825edb1
emmanuelcadete <engmecanicocadete@gmail.com>
2024-07-07 12:17:58 -0300

    linda

--- views/recpcao/index.php
+++ views/recpcao/index.php
@@ -1,7 +1,8 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/sessao.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/conexao.php';
-if ($_SESSION['funcao'] != '0') {
+
+if ($_SESSION['funcao'] != '0' && $_SESSION['name_sistema'] != "RECEPCAO") {
     echo '<script>window.history.back()</script>';
     exit();
 }
