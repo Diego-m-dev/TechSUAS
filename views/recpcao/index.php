5a82b3b01811261dd81be35c2716cd03fed28abe
MatthsDev <137323572+MatthsDev@users.noreply.github.com>
2024-07-08 09:36:30 -0300

    s

--- views/recpcao/index.php
+++ views/recpcao/index.php
@@ -16,7 +16,8 @@ if ($_SESSION['funcao'] != '0' && $_SESSION['name_sistema'] != "RECEPCAO") {
 
     <link rel="stylesheet" href="/TechSUAS/css/recepcao/style.css">
     <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -94,8 +95,11 @@ if ($_SESSION['funcao'] != '0' && $_SESSION['name_sistema'] != "RECEPCAO") {
                 </form>
             </div>
         </div>
-        
-    </div>
+
+
+
+        <!-- FOOTER DA PAGINA -->
+        <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/footer.php'; ?>
 
         <div class="drop-all">
             <div class="menu-drop">
@@ -116,188 +120,182 @@ if ($_SESSION['funcao'] != '0' && $_SESSION['name_sistema'] != "RECEPCAO") {
                     <?php } ?>
                 </div>
             </div>
+        </div>
 
-    <script>
-        $(document).ready(function () {
-            var modal = document.getElementById("myModal");
-            var span = document.getElementsByClassName("close")[0];
-            var modalTriggers = document.querySelectorAll(".modal-trigger");
+        <script>
+            $(document).ready(function () {
+                var modal = document.getElementById("myModal");
+                var span = document.getElementsByClassName("close")[0];
+                var modalTriggers = document.querySelectorAll(".modal-trigger");
 
-<<<<<<< HEAD
-            modalTriggers.forEach(function (trigger) {
-                trigger.addEventListener("click", function (event) {
-                    event.preventDefault();
-                    // Obtém o texto do botão ignorando os filhos (ícones)
-                    var buttonText = this.innerText.trim().split("\n").pop().trim();
-                    document.getElementById("modalText").innerText = buttonText;
-                    modal.style.display = "block";
+                modalTriggers.forEach(function (trigger) {
+                    trigger.addEventListener("click", function (event) {
+                        event.preventDefault();
+                        // Obtém o texto do botão ignorando os filhos (ícones)
+                        var buttonText = this.innerText.trim().split("\n").pop().trim();
+                        document.getElementById("modalText").innerText = buttonText;
+                        modal.style.display = "block";
+                    });
                 });
-            });
 
-            span.onclick = function () {
-                modal.style.display = "none";
-            }
-
-            window.onclick = function (event) {
-                if (event.target == modal) {
+                span.onclick = function () {
                     modal.style.display = "none";
                 }
-            }
 
-            // Adiciona o evento blur ao campo CPF
-            $("#cpf").on("blur", function () {
-                validarCPF(this);
-            });
-
-            $("#cpfForm").on("submit", function (event) {
-                var cpfInput = document.getElementById("cpf");
-                if (!validarCPF(cpfInput)) {
-                    event.preventDefault(); // Impede o envio do formulário se o CPF for inválido
-                }
-            });
-        });
-
-        function validarCPF(el) {
-            if (!_cpf(el.value)) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'CPF Inválido',
-                    text: 'Por favor, insira um CPF válido!',
-                    confirmButtonText: 'OK'
-                });
-
-                // Apaga o valor
-                el.value = "";
-                return false;
-            }
-            return true;
-        }
-        document.getElementById('cpf').addEventListener('blur', function () {
-            const cpf = this.value.replace(/[^\d]/g, ''); // Remove caracteres não numéricos
-
-            if (cpf) {
-                const xhr = new XMLHttpRequest();
-                xhr.open('POST', '/TechSUAS/controller/cadunico/fichario/buscar.php', true);
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-                // Mostra o elemento de carregamento
-                document.getElementById('loading').style.display = 'block';
-
-                xhr.onreadystatechange = function () {
-                    if (xhr.readyState === 4) {
-                        // Esconde o elemento de carregamento
-                        document.getElementById('loading').style.display = 'none';
-
-                        if (xhr.status === 200) {
-                            const response = JSON.parse(xhr.responseText);
-                            if (response.success) {
-                                document.getElementById('nome').value = response.nome;
-                                document.getElementById('cod').value = response.cod_fam_familia;
-                            } else {
-                                alert('CPF ' + cpf + ' não encontrado');
-                            }
-                        } else {
-                            alert('Erro ao processar a requisição.');
-                        }
-                    }
-                };
-                xhr.send('cpf=' + cpf);
-            }
-        });
-=======
-            <script>
-                $(document).ready(function() {
-                    var modal = document.getElementById("myModal");
-                    var span = document.getElementsByClassName("close")[0];
-                    var modalTriggers = document.querySelectorAll(".modal-trigger");
-
-                    modalTriggers.forEach(function(trigger) {
-                        trigger.addEventListener("click", function(event) {
-                            event.preventDefault();
-                            // Obtém o texto do botão ignorando os filhos (ícones)
-                            var buttonText = this.innerText.trim().split("\n").pop().trim();
-                            document.getElementById("modalText").innerText = buttonText;
-                            modal.style.display = "block";
-                        });
-                    });
-
-                    span.onclick = function() {
+                window.onclick = function (event) {
+                    if (event.target == modal) {
                         modal.style.display = "none";
                     }
-
-                    window.onclick = function(event) {
-                        if (event.target == modal) {
-                            modal.style.display = "none";
-                        }
-                    }
-
-                    // Adiciona o evento blur ao campo CPF
-                    $("#cpf").on("blur", function() {
-                        validarCPF(this);
-                    });
-
-                    $("#cpfForm").on("submit", function(event) {
-                        var cpfInput = document.getElementById("cpf");
-                        if (!validarCPF(cpfInput)) {
-                            event.preventDefault(); // Impede o envio do formulário se o CPF for inválido
-                        }
-                    });
-                });
-
-                function validarCPF(el) {
-                    if (!_cpf(el.value)) {
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'CPF Inválido',
-                            text: 'Por favor, insira um CPF válido!',
-                            confirmButtonText: 'OK'
-                        });
-
-                        // Apaga o valor
-                        el.value = "";
-                        return false;
-                    }
-                    return true;
                 }
-                document.getElementById('cpf').addEventListener('blur', function() {
-                    const cpf = this.value.replace(/[^\d]/g, ''); // Remove caracteres não numéricos
 
-                    if (cpf) {
-                        const xhr = new XMLHttpRequest();
-                        xhr.open('POST', '/TechSUAS/controller/cadunico/fichario/buscar.php', true);
-                        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
->>>>>>> 3636c6cfda8a8bd0fe4617bc0118f795640a255a
+                // Adiciona o evento blur ao campo CPF
+                $("#cpf").on("blur", function () {
+                    validarCPF(this);
+                });
 
-                        // Mostra o elemento de carregamento
-                        document.getElementById('loading').style.display = 'block';
-
-<<<<<<< HEAD
-
-    </script>
-=======
-                        xhr.onreadystatechange = function() {
-                            if (xhr.readyState === 4) {
-                                // Esconde o elemento de carregamento
-                                document.getElementById('loading').style.display = 'none';
-
-                                if (xhr.status === 200) {
-                                    const response = JSON.parse(xhr.responseText);
-                                    if (response.success) {
-                                        document.getElementById('nome').value = response.nome;
-                                        document.getElementById('cod').value = response.cod_fam_familia;
-                                    } else {
-                                        alert('CPF ' + cpf + ' não encontrado');
-                                    }
-                                } else {
-                                    alert('Erro ao processar a requisição.');
-                                }
-                            }
-                        };
-                        xhr.send('cpf=' + cpf);
+                $("#cpfForm").on("submit", function (event) {
+                    var cpfInput = document.getElementById("cpf");
+                    if (!validarCPF(cpfInput)) {
+                        event.preventDefault(); // Impede o envio do formulário se o CPF for inválido
                     }
                 });
-            </script>
->>>>>>> 3636c6cfda8a8bd0fe4617bc0118f795640a255a
+            });
+
+            function validarCPF(el) {
+                if (!_cpf(el.value)) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'CPF Inválido',
+                        text: 'Por favor, insira um CPF válido!',
+                        confirmButtonText: 'OK'
+                    });
+
+                    // Apaga o valor
+                    el.value = "";
+                    return false;
+                }
+                return true;
+            }
+            document.getElementById('cpf').addEventListener('blur', function () {
+                const cpf = this.value.replace(/[^\d]/g, ''); // Remove caracteres não numéricos
+
+                if (cpf) {
+                    const xhr = new XMLHttpRequest();
+                    xhr.open('POST', '/TechSUAS/controller/cadunico/fichario/buscar.php', true);
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+                    // Mostra o elemento de carregamento
+                    document.getElementById('loading').style.display = 'block';
+
+                    xhr.onreadystatechange = function () {
+                        if (xhr.readyState === 4) {
+                            // Esconde o elemento de carregamento
+                            document.getElementById('loading').style.display = 'none';
+
+                            if (xhr.status === 200) {
+                                const response = JSON.parse(xhr.responseText);
+                                if (response.success) {
+                                    document.getElementById('nome').value = response.nome;
+                                    document.getElementById('cod').value = response.cod_fam_familia;
+                                } else {
+                                    alert('CPF ' + cpf + ' não encontrado');
+                                }
+                            } else {
+                                alert('Erro ao processar a requisição.');
+                            }
+                        }
+                    };
+                    xhr.send('cpf=' + cpf);
+                }
+            });
+
+            $(document).ready(function () {
+                var modal = document.getElementById("myModal");
+                var span = document.getElementsByClassName("close")[0];
+                var modalTriggers = document.querySelectorAll(".modal-trigger");
+
+                modalTriggers.forEach(function (trigger) {
+                    trigger.addEventListener("click", function (event) {
+                        event.preventDefault();
+                        // Obtém o texto do botão ignorando os filhos (ícones)
+                        var buttonText = this.innerText.trim().split("\n").pop().trim();
+                        document.getElementById("modalText").innerText = buttonText;
+                        modal.style.display = "block";
+                    });
+                });
+
+                span.onclick = function () {
+                    modal.style.display = "none";
+                }
+
+                window.onclick = function (event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+
+                // Adiciona o evento blur ao campo CPF
+                $("#cpf").on("blur", function () {
+                    validarCPF(this);
+                });
+
+                $("#cpfForm").on("submit", function (event) {
+                    var cpfInput = document.getElementById("cpf");
+                    if (!validarCPF(cpfInput)) {
+                        event.preventDefault(); // Impede o envio do formulário se o CPF for inválido
+                    }
+                });
+            });
+
+            function validarCPF(el) {
+                if (!_cpf(el.value)) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'CPF Inválido',
+                        text: 'Por favor, insira um CPF válido!',
+                        confirmButtonText: 'OK'
+                    });
+
+                    // Apaga o valor
+                    el.value = "";
+                    return false;
+                }
+                return true;
+            }
+            document.getElementById('cpf').addEventListener('blur', function () {
+                const cpf = this.value.replace(/[^\d]/g, ''); // Remove caracteres não numéricos
+
+                if (cpf) {
+                    const xhr = new XMLHttpRequest();
+                    xhr.open('POST', '/TechSUAS/controller/cadunico/fichario/buscar.php', true);
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+
+                    // Mostra o elemento de carregamento
+                    document.getElementById('loading').style.display = 'block';
+
+                    xhr.onreadystatechange = function () {
+                        if (xhr.readyState === 4) {
+                            // Esconde o elemento de carregamento
+                            document.getElementById('loading').style.display = 'none';
+
+                            if (xhr.status === 200) {
+                                const response = JSON.parse(xhr.responseText);
+                                if (response.success) {
+                                    document.getElementById('nome').value = response.nome;
+                                    document.getElementById('cod').value = response.cod_fam_familia;
+                                } else {
+                                    alert('CPF ' + cpf + ' não encontrado');
+                                }
+                            } else {
+                                alert('Erro ao processar a requisição.');
+                            }
+                        }
+                    };
+                    xhr.send('cpf=' + cpf);
+                }
+            });
+        </script>
 
 
 </body>
