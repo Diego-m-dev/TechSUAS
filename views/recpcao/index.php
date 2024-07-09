d1113c2660c7d5e7aa0081d0c73deaedfd3bd24d
Diego-m-dev <diegomoreira.pab@gmail.com>
2024-07-09 12:16:46 -0300

    1

--- views/recpcao/index.php
+++ views/recpcao/index.php
@@ -25,7 +25,7 @@ if ($_SESSION['funcao'] != '0' && $_SESSION['name_sistema'] != "RECEPCAO") {
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="/TechSUAS/js/cpfvalid.js"></script>
-    <title>Recpção</title>
+    <title>Recepção</title>
 </head>
 
 <body>
