e0bf4d0dd4b63090147cdc7aef2545bf587bb68e
Diego Moreira <diegomoreira.pab@gmail.com>
2024-07-09 22:45:40 -0300

    w

--- config/css_ind.php
+++ config/css_ind.php
@@ -7,7 +7,7 @@ if ($_SESSION['municipio'] == "9876543") {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Suporte Cadastro Único</title>
+  <title>Suporte Cadastro Único - TechSUAS</title>
   <link rel="stylesheet" href="/TechSUAS/css/cadunico/style_index_cad.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
@@ -21,7 +21,7 @@ if ($_SESSION['municipio'] == "9876543") {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SBU Cadastro Único </title>
+  <title>SBU Cadastro Único - TechSUAS</title>
   <link rel="stylesheet" href="/TechSUAS/css/cadunico/style_index_cad.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
