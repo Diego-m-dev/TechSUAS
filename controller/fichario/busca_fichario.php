823c28bc5736a268918080fa577a0800e11f60ab
emmanuelcadete <engmecanicocadete@gmail.com>
2024-07-12 01:36:39 -0300

    tyytd

--- controller/fichario/busca_fichario.php
+++ controller/fichario/busca_fichario.php
@@ -8,7 +8,7 @@ if (isset($_POST['arm'])) {
 
   $arm = $_POST['arm'];
 
-  $stmt_ficha = $pdo->prepare("SELECT pas, gav, arm FROM ficharios WHERE arm = :arm");
+  $stmt_ficha = $pdo->prepare("SELECT pas, gav, arm FROM ficharios WHERE arm = :arm ORDER BY gav, pas");
   $stmt_ficha->execute(array(':arm' => $arm));
   
   if ($stmt_ficha->rowCount() != 0) {
@@ -25,7 +25,19 @@ if (isset($_POST['arm'])) {
 
       if ($sql_codfam_query->num_rows > 0) {
         $dados_codfam = $sql_codfam_query->fetch_assoc();
-        $codfam = $dados_codfam['codfam'];
+        $codfam_1 = $dados_codfam['codfam'];
+        $cpf_limpo = preg_replace('/\D/', '', $codfam_1);
+        $ajustando_cod = str_pad($cpf_limpo, 11,'0',STR_PAD_LEFT);
+    
+          $sql_tudo = "SELECT cod_familiar_fam FROM tbl_tudo WHERE cod_familiar_fam = '$ajustando_cod' AND cod_parentesco_rf_pessoa = 1";
+          $sql_tudo_query = $conn->query($sql_tudo) or die("Erro na preparação COD: Y909X");
+          if ($sql_tudo_query->num_rows > 0) {
+            $dados_tudo = $sql_tudo_query->fetch_assoc();
+            $codfam = $dados_tudo['cod_familiar_fam'];
+          } else {
+            $codfam = "V7 - " . $codfam_1 . " - V7";
+          }
+
       } else {
         $codfam = '';
       }
