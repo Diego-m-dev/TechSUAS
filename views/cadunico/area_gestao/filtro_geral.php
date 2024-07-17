2df9c5caba5d8d092d8b3691466c55e3fd92150c
emmanuelcadete <engmecanicocadete@gmail.com>
2024-07-16 23:13:32 -0300

    geaar

--- views/cadunico/area_gestao/filtro_geral.php
+++ views/cadunico/area_gestao/filtro_geral.php
@@ -7,6 +7,17 @@ if ($_SESSION['funcao'] != '1') {
     echo '<script>window.history.back()</script>';
     exit();
 }
+
+$stmt_dt_atual = "SELECT DISTINCT YEAR(dat_atual_fam) AS ano_atual FROM tbl_tudo ORDER BY ano_atual DESC";
+$stmt_dt_atual_query = $conn->query($stmt_dt_atual);
+if (!$stmt_dt_atual_query) {
+  die("ERRO ao consultar! " . $conn->error);
+}
+
+$anos_atual = [];
+while ($b = $stmt_dt_atual_query->fetch_assoc()) {
+  $anos_atual[] = $b['ano_atual'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -40,7 +51,7 @@ if ($_SESSION['funcao'] != '1') {
     <div class="container">
         <div class="blocs">
             <div class="bloc">
-                <div class=bloc1>
+                <div class="bloc1">
                     <label for="columns-select">Selecione as Colunas:</label>
                     <select id="columns-select" multiple onchange="criarTabela([])">
                         <option value="0">Código Familiar</option>
@@ -48,7 +59,7 @@ if ($_SESSION['funcao'] != '1') {
                         <option value="2">NIS</option>
                         <option value="3">Data de Nascimento</option>
                         <option value="4">Idade</option>
-                        <option value="5">Renda Per Capita</option>
+                        <option value="5">Renda Total</option>
                         <option value="6">Endereço</option>
                         <option value="7">Status</option>
                         <option value="8">Escola</option>
@@ -60,23 +71,25 @@ if ($_SESSION['funcao'] != '1') {
                 </div>
                 <div>
                     <div>
-                        <label for="">Filtrar por Nome:</label>
-                        <input type="text" id="" onkeyup="" />
+                        <label for="">Nome:</label>
+                        <input type="text" id="nome_pess" onkeyup="aplicarFiltros()" />
                     </div>
                     <div>
-                        <label for="cod_fam">Filtrar por Código Familiar:</label>
+                        <label for="cod_fam">Código Familiar:</label>
                         <input type="text" id="cod_fam" onkeyup="aplicarFiltros()" />
                     </div>
+                    <label for="renda_per">Renda Per Capita:</label>
+                    <input type="number" id="renda_per" onkeyup="aplicarFiltros()">
 
                     <div>
-                        <label for="filtro_status">Filtrar por Status:</label>
+                        <label for="filtro_status">Status:</label>
                         <select id="filtro_status" onchange="aplicarFiltros()">
                             <option value="">Todos</option>
                             <option value="ATUALIZADA">Atualizado</option>
                             <option value="DESATUALIZADO">Desatualizado</option>
                         </select>
                         <div>
-                            <label for="filtro-mh">Filtrar por Sexo:</label>
+                            <label for="filtro-mh">Sexo:</label>
                             <select id="filtro-mh" onchange="aplicarFiltros()">
                                 <option value="">Todos</option>
                                 <option value="MULHER">MULHERES</option>
@@ -90,11 +103,11 @@ if ($_SESSION['funcao'] != '1') {
             </div>
             <div class="bloc2">
                     <div>
-                        <label for="#">Filtrar por Cpf:</label>
-                        <input type="text" id="" onkeyup="" />
+                        <label for="cpf_pess">CPF:</label>
+                        <input type="text" id="cpf_pess" onkeyup="aplicarFiltros()" />
                     </div>
                 <div>
-                    <label for="filtro-grupo">Filtrar por Escola:</label>
+                    <label for="filtro-grupo">Escola:</label>
                     <input type="text" id="filtro-grupo" onchange="aplicarFiltros()" placeholder="Filtro por Escola" />
                 </div>
 
@@ -104,12 +117,22 @@ if ($_SESSION['funcao'] != '1') {
                 </div>
 
                 <div>
-                    <label for="filtro-idade">Filtrar por Idade:</label>
+                    <label for="filtro-idade">Idade:</label>
                     <input type="number" id="filtro-idade" onkeyup="aplicarFiltros()" placeholder="Filtro por idade" />
                 </div>
 
+                <label for="ano_atual">Ano da Ultima Atualização:</label>
+                <select id="filtro_ano" onchange="aplicarFiltros()">
+                  <option value="">Filtro pelo ano de Atualização</option>
+<?php
+  foreach ($anos_atual as $ano) {
+    echo "<option value'$ano'>$ano</option>";
+  }
+?>
+                </select>
+
                 <div>
-                    <label for="filtro-parent">Filtrar por Parentesco:</label>
+                    <label for="filtro-parent">Parentesco:</label>
                     <select id="filtro-parent" onchange="aplicarFiltros()">
                         <option value="">Filtro por parentesco</option>
                         <option value="RESPONSAVEL FAMILIAR">RESPONSAVEL FAMILIAR</option>
@@ -129,7 +152,7 @@ if ($_SESSION['funcao'] != '1') {
         </div>
         <div class="btns">
             <div>
-            <button id="filtroCriaIdosButton" onclick="filtroCriaIdos()">Filtrar Dados</button>
+            <button id="filtroCriaIdosButton" onclick="filtroCriaIdos()">Buscar Dados</button>
                 <button onclick="window.location.href='/TechSUAS/views/cadunico/area_gestao/filtro_geral'">Limpar dados</button>
             </div>
             <div><button onclick="voltarFiltros()">Voltar</button></div>
@@ -139,6 +162,11 @@ if ($_SESSION['funcao'] != '1') {
 
         <div id="tabela-dinamica"></div>
     </div>
+    <script>
+      $(document).ready(function () {
+        $('#cpf_pess').mask('000.000.000-00')
+      })
+    </script>
 </body>
 
 </html>
