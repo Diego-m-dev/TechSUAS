2285cb93f7a9a1b3182ead139a0ab4e0f981c5e8
emmanuelcadete <engmecanicocadete@gmail.com>
2024-08-07 15:51:28 -0300

    OEIJ4

--- controller/fichario/busca_fichario.php
+++ controller/fichario/busca_fichario.php
@@ -12,17 +12,21 @@ if (isset($_POST['arm'])) {
       f.pas, f.gav, f.arm, 
       COALESCE(t.cod_familiar_fam, CONCAT('V7 - ', fich.codfam, ' - V7')) AS codfam
     FROM ficharios f
+
     LEFT JOIN (
       SELECT codfam, 
             CONVERT(arm_gav_pas USING utf8mb3) COLLATE utf8mb3_general_ci AS arm_gav_pas
       FROM fichario
     ) fich
+
     ON CONVERT(CONCAT(LPAD(f.arm, 2, '0'), ' - ', LPAD(f.gav, 2, '0'), ' - ', LPAD(f.pas, 3, '0')) USING utf8mb3) COLLATE utf8mb3_general_ci = fich.arm_gav_pas
+
     LEFT JOIN (
       SELECT cod_familiar_fam 
       FROM tbl_tudo 
       WHERE cod_parentesco_rf_pessoa = 1
     ) t 
+
     ON t.cod_familiar_fam = LPAD(REPLACE(fich.codfam, '\D', ''), 11, '0')
     WHERE f.arm = :arm
     ORDER BY f.gav, f.pas
