1481cbb0ecde44006401126c3e4f41377d03ff49
emmanuelcadete <engmecanicocadete@gmail.com>
2024-09-09 15:30:36 -0300

    123

--- proj_pedidos_cleiton/salvar_pedido.php
+++ proj_pedidos_cleiton/salvar_pedido.php
@@ -1,5 +1,31 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/conexao.php';
+// Conecte-se ao seu banco de dados MySQL usando as credenciais adequadas.
+$host = '89.117.7.52';
+$usuario = 'u198416735_techsuas';
+$senha = 'GNoY;y#6Dv6#';
+$banco = 'u198416735_tachsuas';
+$port = 3306;
+
+$raiz_dom = "/TechSUAS/";
+
+date_default_timezone_set('America/Sao_Paulo');
+try {
+    // Adicione a porta à string DSN
+    $pdo = new PDO("mysql:dbname=$banco;host=$host;port=$port", "$usuario", "$senha", array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ));
+    // Conexão MySQLi para o backup
+    $conn = mysqli_connect($host, $usuario, $senha, $banco, $port);
+    
+    if ($conn->connect_error) {
+        die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+    } else {
+    }
+} catch (Exception $e) {
+    echo "Erro ao conectar com o banco de dados! " . $e->getMessage();
+    exit();
+}
 
 // Recebe os dados do pedido
 $tipo = $_POST['tipo'];
