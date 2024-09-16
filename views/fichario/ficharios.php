7ed8e2fb9775ed35cb1a848609922162b7b43eb5
emmanuelcadete <engmecanicocadete@gmail.com>
2024-09-16 00:37:12 -0300

    cno

--- views/fichario/ficharios.php
+++ views/fichario/ficharios.php
@@ -13,7 +13,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/permissao_cadunico.ph
 
     <link rel="website icon" type="png" href="/TechSUAS/img/geral/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="/TechSUAS/css/fichario/style_arquivo.css">
+    <link rel="stylesheet" href="/TechSUAS/css/cadunico/fichario/style_arquivo.css">
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -23,25 +23,27 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/TechSUAS/config/permissao_cadunico.ph
 
     <title>Fichário - TechSUAS</title>
 </head>
+
 <body>
-<div class="conteiner-arm">
-  <form action="" method="POST">
-    <label for="">ARMÁRIO</label>
-    <select name="arm" id="arm" required onchange="mudaArmario()">
-      <option value="" disabled selected hidden>00</option>
-      <?php
-      $sql_ficharios = "SELECT arm FROM ficharios GROUP BY arm ORDER BY arm, pas";
-      $sql_ficharios_query = $conn->query($sql_ficharios) or die("Erro na conexão " . $conn->error);
-      if ($sql_ficharios_query->num_rows > 0) {
-        while ($fichario = $sql_ficharios_query->fetch_assoc()) {
-          echo '<option value="' . $fichario['arm'] . '">' . $fichario['arm'] . '</option>';
-        }
-      }
-  $conn_1->close();
-      ?>
-    </select>
-  </form>
-</div>
-<div id="tabela-container"></div>
+    <div class="conteiner-arm">
+        <form action="" method="POST">
+            <label for="arm">ARMÁRIO</label>
+            <select name="arm" id="arm" required onchange="mudaArmario()">
+                <option value="" disabled selected hidden>00</option>
+                <?php
+                $sql_ficharios = "SELECT arm FROM ficharios GROUP BY arm ORDER BY arm, pas";
+                $sql_ficharios_query = $conn->query($sql_ficharios) or die("Erro na conexão " . $conn->error);
+                if ($sql_ficharios_query->num_rows > 0) {
+                    while ($fichario = $sql_ficharios_query->fetch_assoc()) {
+                        echo '<option value="' . $fichario['arm'] . '">' . $fichario['arm'] . '</option>';
+                    }
+                }
+                $conn_1->close();
+                ?>
+            </select>
+        </form>
+    </div>
+    <div id="tabela-container"></div>
 </body>
+
 </html>
