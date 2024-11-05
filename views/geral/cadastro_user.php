1fc81bbe6664494e55c0844b93ca2779b3810fee
emmanuelcadete <engmecanicocadete@gmail.com>
2024-11-05 06:09:11 -0300

    serio

--- views/geral/cadastro_user.php
+++ views/geral/cadastro_user.php
@@ -30,76 +30,84 @@ if ($_SESSION['funcao'] != '1' && $_SESSION['funcao'] != '0')
         </h1>
     </div>
 
-  <div class="container">
-    <form method="post" action="/TechSUAS/controller/geral/processo_cad_user.php">
-
+  <form method="post" action="/TechSUAS/controller/geral/cad_user.php">
+    <div class="container">
     <input type="hidden" name="cpf" value="<?php echo $_SESSION['municipio']; ?>">
-      <div class="nome">
-          <label>Nome completo:</label>
-          <input type="text" class="nome" name="nome_user" placeholder="Sem Abreviação." required style="width: 300px;">
-      </div>
-      <div class="email">
-          <label>E-mail:</label>
-          <input type="email" name="email" placeholder="Digite aqui seu e-mail." required style="width: 300px;">
-      </div>
-      <div class="setor">
+
+    <div class="nome">
+      <label>Nome completo:</label>
+      <input type="text" class="nome" name="nome_user" placeholder="Sem Abreviação." required style="width: 300px;">
+    </div>
+
+    <div class="email">
+      <label>E-mail:</label>
+      <input type="email" name="email" placeholder="Digite aqui seu e-mail." required style="width: 300px;">
+    </div>
+
+    <div class="setor">
     <label>Setor:</label>
     <select name="setor" required>
       <option value="" disabled selected hidden>Selecione</option>
-<?php
-$idSistema = $_SESSION['sistema_id'];
-  $sql_setor = "SELECT * FROM sistemas WHERE id = '$idSistema'";
-  $sql_setor_query = $conn_1->query($sql_setor) or die("Erro ". $conn_1 - error);
+      <?php
+                        $idSistema = $_SESSION['sistema_id'];
+                        $sql_setor = "SELECT * FROM sistemas WHERE id = '$idSistema'";
+                        $sql_setor_query = $conn_1->query($sql_setor) or die("Erro ". $conn_1 - error);
 
-  if ($sql_setor_query->num_rows > 0 ) {
-    $bds = $sql_setor_query->fetch_assoc();
-    $idSetor = $bds['setores_id'];
-    $consultaSetores = $conn_1->query("SELECT instituicao, nome_instit FROM setores WHERE id = '$idSetor'");
+                        if ($sql_setor_query->num_rows > 0 ) {
+                        $bds = $sql_setor_query->fetch_assoc();
+                        $idSetor = $bds['setores_id'];
+                        $consultaSetores = $conn_1->query("SELECT instituicao, nome_instit FROM setores WHERE id = '$idSetor'");
 
-// Verifica se há resultados na consulta
-if ($consultaSetores->num_rows > 0) {
+                        // Verifica se há resultados na consulta
+                        if ($consultaSetores->num_rows > 0) {
 
-    // Loop para criar as opções do select
-    while ($setor = $consultaSetores->fetch_assoc()) {
-        echo '<option value="' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '">' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '</option>';
-    }
-}
-  } elseif ($_SESSION['funcao'] == "0") {
-    $consultaSetores = $conn_1->query("SELECT instituicao, nome_instit FROM setores ");
+                        // Loop para criar as opções do select
+                        while ($setor = $consultaSetores->fetch_assoc()) {
+                            echo '<option value="' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '">' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '</option>';
+                            }
+                          }
+                        } elseif ($_SESSION['funcao'] == "0") {
+                        $consultaSetores = $conn_1->query("SELECT instituicao, nome_instit FROM setores ");
 
-// Verifica se há resultados na consulta
-  if ($consultaSetores->num_rows > 0) {
+                        // Verifica se há resultados na consulta
+                        if ($consultaSetores->num_rows > 0) {
 
-      // Loop para criar as opções do select
-    while ($setor = $consultaSetores->fetch_assoc()) {
-      echo '<option value="' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '">' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '</option>';
-    }
-  }
-}
-$conn-close();
+                          // Loop para criar as opções do select
+                        while ($setor = $consultaSetores->fetch_assoc()) {
+                          echo '<option value="' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '">' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '</option>';
+                            }
+                          }
+                        }
+$conn->close();
 ?>
     </select>
-</div>
+  </div>
+
   <div class="tipodeacesso">
     <label>Função: </label>
-      <select name="funcao" id="funcao" required onchange="mostrarCampoTexto()">
-        <option value="" disabled selected hidden>Selecione</option>
-        <option value="1">Gestão</option>
-        <option value="2">Tecnico(a)</option>
-        <option value="3">Outros</option>
-      </select>
+    <select name="funcao" id="funcao" required onchange="mostrarCampoTexto()">
+      <option value="" disabled selected hidden>Selecione</option>
+      <option value="1">Gestão</option>
+      <option value="2">Tecnico(a)</option>
+      <option value="3">Outros</option>
+    </select>
 
-        <input type="text" name="funcao_outros" id="funcao_outros" style="display: none;" placeholder="Digite a função">
-      <div class="btns">
-          <button type="submit">Cadastrar</button>
-          <a href="/TechSUAS/config/back">
-              <i class="fas fa-arrow-left"></i> Voltar ao menu
-          </a>
-      </div>
-      </div>
-  </form>
+    <input type="text" name="funcao_outros" id="funcao_outros" style="display: none;" placeholder="Digite a função">
+
+  </div>
+
     </div>
-    <script>
+
+    <div class="btns">
+      <button type="submit">Cadastrar</button>
+        <a href="/TechSUAS/config/back">
+          <i class="fas fa-arrow-left"></i> Voltar ao menu
+        </a>
+    </div>
+
+  </form>
+
+  <script>
     function mostrarCampoTexto() {
         var select = document.getElementById("funcao");
         var campoTexto = document.getElementById("funcao_outros");
@@ -112,6 +120,7 @@ $conn-close();
             campoTexto.style.display = "none";
         }
     }
-</script>
+  </script>
+  <?php $conn_1->close(); ?>
 </body>
 </html>
