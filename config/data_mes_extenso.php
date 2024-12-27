dbd961bbbcece1028583e72ce52b895e8f67976d
Diego Cadete <engmecanicocadete@gmail.com>
2024-12-26 23:41:56 -0300

    bem

--- config/data_mes_extenso.php
+++ config/data_mes_extenso.php
@@ -15,7 +15,7 @@ $data_formatada_extenso = $formatter->format($date_formatando);
 $data_cabecalho = date('d/m/Y H:i');
 $ibge_cod = $_SESSION['municipio'];
 $stmt_city = "SELECT municipio, estado FROM municipios WHERE cod_ibge = '$ibge_cod'";
-$stmt_city_query = $conn_1->query($stmt_city) or die("Erro " .$conn_1 - error);
+$stmt_city_query = $conn_1->query($stmt_city) or die("Erro " . $conn_1 -> error);
 
   if ($stmt_city_query->num_rows > 0) {
     $sfinge = $stmt_city_query->fetch_assoc();
